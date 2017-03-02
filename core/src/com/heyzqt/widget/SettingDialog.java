9a1ff1d3118d9aedcd72a3af6494dc16a27a05e0
heyzqt <729224643@qq.com>
2017-03-02 13:23:08 +0800

    initial commit SettingDialog.java

---
+++ core/src/com/heyzqt/widget/SettingDialog.java
@@ -0,0 +1,17 @@
+package com.heyzqt.widget;
+
+/**
+ * Created by heyzqt on 2017/3/2.
+ *
+ * 设置界面关于我们对话框
+ */
+public class SettingDialog {
+
+	private float x;
+	private float y;
+
+	public SettingDialog(float x, float y) {
+		this.x = x;
+		this.y = y;
+	}
+}
